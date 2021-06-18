44e1fc306a131042d5043558afa23c58ac3c0084
刘舰泽 <mr.ljz@qq.com>
2021-06-18 16:38:24 +0800

    系统控制页面

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -34,7 +34,8 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        // target: `http://localhost:8080`,
+        target: `http://218.244.136.178:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
